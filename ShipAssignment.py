52554746397d832d07be140aabfc4fe932a6f7ba
Fabián Sepúlveda <fabiane@datawalt.cl>
2023-07-27 18:55:41 -0400

    ShipAssignement add libs _

--- ShipAssignment.py
+++ ShipAssignment.py
@@ -1,5 +1,6 @@
 from pyomo.environ import ConcreteModel
 import pyomo.environ as pyo
+from pyomo.environ import ConcreteModel, Set, RangeSet, Var, Binary, Objective, minimize, Constraint, SolverFactory, TerminationCondition
 
 
 class ModeloPlanificacionBarcos:
