2d14b43f7aef9736dd7ebaf4e429535071329e26
Fabián Sepúlveda <fabiane@datawalt.cl>
2023-07-28 10:39:05 -0400

    ShipAssignement cambios 6

---
+++ Modelo2.py
@@ -0,0 +1,133 @@
+from pyomo.environ import ConcreteModel
+import pyomo.environ as pyo
+from pyomo.environ import ConcreteModel, Set, RangeSet, Var, Binary, Objective, minimize, Constraint, SolverFactory, TerminationCondition
+import pandas as pd 
+
+class ModeloPlanificacionBarcosBeta:
+    def __init__(self, N, D, T, P, A, R, personas, max_dias_consecutivos):
+        self.N = N
+        self.D = D
+        self.T = T
+        self.P = P
+        self.A = A
+        self.R = R
+        self.personas = personas
+        self.max_dias_consecutivos = max_dias_consecutivos
+        self.modelo = self.crear_modelo()
+
+    def crear_modelo(self):
+        m = ConcreteModel()
+
+        m.I = Set(initialize=self.personas.keys())  # personas
+        m.J = RangeSet(1, self.N)  # barcos
+        m.K = RangeSet(1, self.D)  # días
+        m.R = Set(initialize=self.R[0].keys())  # roles
+
+        m.X = Var(m.I, m.J, m.K, within=Binary)
+        m.Y = Var(m.I, m.J, m.K, within=Binary)
+        m.Z = Var(within=NonNegativeIntegers)
+
+        def regla_objetivo(modelo):
+            return modelo.Z
+        m.OBJ = Objective(rule=regla_objetivo, sense=minimize)
+
+        # Updated constraints
+        def regla_requisito_rol(modelo, j, r, k):
+            return sum(modelo.X[i, j, k] for i in modelo.I if self.personas[i] == r) >= self.R[j - 1][r]
+
+        def regla_dias_consecutivos(modelo, i, j, k):
+            if k <= modelo.K - self.T + 1:
+                return sum(modelo.X[i, j, kp] for kp in range(k, min(k + self.T, self.D+1))) - self.T * modelo.Y[i, j, k] >= 0
+            return Constraint.Skip
+
+        def regla_dias_descanso(modelo, i, k):
+            if k <= self.D - self.T - self.P:
+                return sum(modelo.X[i, j, t] for t in range(k + self.T + 1, k + self.T + self.P + 1) for j in modelo.J) <= self.P * (1 - sum(modelo.Y[i, j, k] for j in modelo.J))
+            return Constraint.Skip
+
+        def regla_asignacion_mismo_dia(modelo, i, k):
+            return sum(modelo.X[i, j, k] for j in modelo.J) <= 1
+
+        def regla_max_dias_consecutivos(modelo, i):
+            return sum(modelo.X[i, j, k] for j in modelo.J for k in range(1, self.D + 1)) <= self.max_dias_consecutivos
+
+        def regla_disponibilidad(modelo, i, j, k):
+            return modelo.X[i, j, k] <= self.A[i - 1][k - 1]
+
+        def regla_max_workload(modelo, i):
+            return sum(modelo.X[i, j, k] for j in modelo.J for k in modelo.K) <= modelo.Z
+
+        m.requisito_rol = Constraint(m.J, m.R, m.K, rule=regla_requisito_rol)
+        #m.dias_consecutivos = Constraint(m.I, m.J, m.K, rule=regla_dias_consecutivos)
+        m.dias_descanso = Constraint(m.I, m.K, rule=regla_dias_descanso)
+        m.asignacion_mismo_dia = Constraint(m.I, m.K, rule=regla_asignacion_mismo_dia)
+        m.max_dias_consecutivos = Constraint(m.I, rule=regla_max_dias_consecutivos)
+        m.disponibilidad = Constraint(m.I, m.J, m.K, rule=regla_disponibilidad)
+        m.max_workload = Constraint(m.I, rule=regla_max_workload)
+
+        return m
+
+    def solver(self):
+        solver = SolverFactory('glpk')
+        self.resultados = solver.solve(self.modelo)
+
+    def imprimir_resultados(self):
+        if self.resultados.solver.termination_condition == TerminationCondition.optimal:
+            print("Valor Objetivo: ", self.modelo.OBJ())
+            for i in self.modelo.I:
+                for j in self.modelo.J:
+                    for k in self.modelo.K:
+                        if self.modelo.X[i, j, k].value > 0:
+                            print(f"Persona {i} (Rol: {self.personas[i]}) asignada al barco {j} el día {k}.")
+        else:
+            print("No se encontró una solución óptima.")
+
+    def resultados_dataframe(self):
+        """
+        Guarda los resultados de la planificación en un archivo Excel.
+        """
+        if self.resultados.solver.termination_condition == TerminationCondition.optimal:
+            print("Valor Objetivo: ", self.modelo.OBJ())
+
+            # Recopilar datos en una lista de diccionarios
+            data = []
+            for j in self.modelo.J:
+                for i in self.modelo.I:
+                    fila = {"barcos": f"barco {j}", "persona": f"persona {i} (Rol: {self.personas[i]})"}
+                    for k in self.modelo.K:
+                        fila[f"día {k}"] = 1 if self.modelo.X[i, j, k].value >= 0.5 else 0
+                    data.append(fila)
+
+            # Crear DataFrame
+            df = pd.DataFrame(data)
+            df = df.set_index(["barcos", "persona"])
+
+            # Guardar DataFrame en un archivo Excel
+            df.to_excel("planificacion_barcos.xlsx")
+        else:
+            print("No se encontró una solución óptima.")
+
+    def resultados_dataframe_streamlit(self):
+        """
+        Returns the results of the planning as a pandas DataFrame.
+        """
+        if self.resultados.solver.termination_condition == TerminationCondition.optimal:
+            print("Valor Objetivo: ", self.modelo.OBJ())
+
+            # Collect data in a list of dictionaries
+            data = []
+            for j in self.modelo.J:
+                for i in self.modelo.I:
+                    fila = {"barcos": f"barco {j}", "persona": f"persona {i} (Rol: {self.personas[i]})"}
+                    for k in self.modelo.K:
+                        fila[f"día {k}"] = 1 if self.modelo.X[i, j, k].value >= 0.5 else 0
+                    data.append(fila)
+
+            # Create DataFrame
+            df = pd.DataFrame(data)
+            df = df.set_index(["barcos", "persona"])
+
+            return df
+        else:
+            print("No se encontró una solución óptima.")
+            return None
---
+++ pages/Modelo minimizar carga trabajo.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+from Modelo2 import ModeloPlanificacionBarcosBeta
+from pyomo.environ import ConcreteModel, Set, RangeSet, Var, Binary, Objective, minimize, Constraint, SolverFactory, TerminationCondition
+import pandas as pd 
+
+# 0. Título
+st.title('Función Objetivo: Minimizar carga de trabajo')
+
+# 1. Datos de entrada
+st.subheader('Parámetros iniciales')
+
+#N = st.number_input('Ingrese el número total de barcos', min_value=1)
+D = st.number_input('Ingrese el número total de días en el período de planificación', min_value=1)
+T = st.number_input('Ingrese el número de días consecutivos de trabajo en un barco', min_value=1)
+P = st.number_input('Ingrese el número mínimo de días de descanso después de trabajar en un barco durante días consecutivos', min_value=1)
+max_dias_consecutivos = st.number_input('Ingrese el número máximo de días consecutivos que una persona puede trabajar', min_value=1)
+
+# Initialize input data
+A, R, personas = None, None, None
+
+archivo_subido_A = st.sidebar.file_uploader("Subir archivo CSV de Matriz de Disponibilidad", type="csv")
+if archivo_subido_A is not None:
+    A = pd.read_csv(archivo_subido_A).values.tolist()
+    st.subheader('Matriz de Disponibilidad por persona')
+    st.write(pd.DataFrame(A))  # Muestra la tabla de disponibilidad por persona 
+
+archivo_subido_R = st.sidebar.file_uploader("Subir archivo CSV de Requisitos de Roles", type="csv")
+if archivo_subido_R is not None:
+    R = pd.read_csv(archivo_subido_R).to_dict('records')
+    st.subheader('Requisitos de Roles por barco')
+    st.write(pd.DataFrame(R))  # Muestra la tabla de requisitos de roles
+
+archivo_subido_personas = st.sidebar.file_uploader("Subir archivo CSV de Asignaciones de Roles", type="csv")
+if archivo_subido_personas is not None:
+    personas_df = pd.read_csv(archivo_subido_personas)
+    personas = personas_df.set_index(personas_df.columns[0]).iloc[:, 0].to_dict()
+    st.subheader('Cargos por persona')
+    st.write(personas_df)  # Muestra la tabla de asignaciones de roles
+
+# 2. Ejecución
+if st.button('Ejecutar Modelo'):
+    if A is not None and R is not None and personas is not None:
+        modelo = ModeloPlanificacionBarcosBeta(2, D, T, P, A, R, personas, max_dias_consecutivos)
+        modelo.solver()
+
+        # 3. Resultados
+        if modelo.resultados.solver.termination_condition == TerminationCondition.optimal:
+            df = modelo.resultados_dataframe_streamlit()
+
+            # Function to apply background color to non-zero cells
+            def highlight_non_zero(val):
+                if val != 0:
+                    return 'background-color: rgb(230, 255, 230)'  # Light pale green
+
+                return ''
+
+            # Apply the style to the dataframe
+            styled_df = df.style.applymap(highlight_non_zero)
+
+            # Display the styled dataframe
+            st.dataframe(styled_df)
+        else:
+            st.write("No se encontró una solución óptima.")
+    else:
+        st.write("Por favor, cargue todos los archivos requeridos.")
